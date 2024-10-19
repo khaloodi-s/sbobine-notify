2733d3fa5f22a618b7c1d2de9938138e10479132
Khaled <khaled.shahm@gmail.com>
2024-10-20 01:05:01 +0200

    Update connectToWhatsApp.js

--- connectToWhatsApp.js
+++ connectToWhatsApp.js
@@ -1,10 +1,9 @@
-// require('dotenv').config(); THIS IS FOR LOCAL TESTING PURPOSES ONLY - COMMENT IT OUT BEFORE DEPLOYING
 const fs = require('fs');
 const path = require('path');
 const { default: makeWASocket, useMultiFileAuthState, DisconnectReason } = require('baileys');
 
 // Path for local storage on Fly.io
-const sessionFilePath = path.join('/data', 'auth_info.json'); 
+const sessionPath = path.join(__dirname, '/data', 'auth_info_baileys'); 
 let sock; // Declare sock at a higher scope to manage the connection
 
 async function connectToWhatsApp() {
@@ -12,19 +11,7 @@ async function connectToWhatsApp() {
         return sock; // Return existing socket if already connected
     }
 
-    let state;
-
-    // Check if the session file exists and load it if it does
-    // if (fs.existsSync(sessionFilePath)) {
-        // state = JSON.parse(fs.readFileSync(sessionFilePath, 'utf8'));
-        // console.log(`Loaded existing session from local storage found at ${sessionFilePath}.`);
-    //} else {
-        //console.log('No existing session found. Please scan the QR code to authenticate.');
-    //}
-
-    const { state: authState, saveCreds } = await useMultiFileAuthState(path.join('/data', 'auth_info_baileys'), {
-        state,
-    });
+    const { state: authState, saveCreds } = await useMultiFileAuthState(sessionPath);
 
     sock = makeWASocket({
         auth: authState,
@@ -41,19 +28,16 @@ async function connectToWhatsApp() {
 
             if (shouldReconnect) {
                 sock = null; // Reset sock to allow reconnection
-                connectToWhatsApp(); // Attempt to reconnect
+                setTimeout(connectToWhatsApp, 5000); // Attempt to reconnect after 5 seconds
             }
         } else if (connection === 'open') {
             console.log('Successfully opened connection');
         }
     });
 
-    sock.ev.on('creds.update', async () => {
-        await saveCreds(); // Save credentials to local storage
-        fs.writeFileSync(sessionFilePath, JSON.stringify(authState), 'utf8'); // Save to local file
-        console.log('Credentials saved to local storage.');
-    });
+    sock.ev.on('creds.update', saveCreds); // Save credentials to local storage automatically
 
+    // Delay sending the first message by 30 seconds
     setTimeout(async () => {
         const jid = '96565022680@s.whatsapp.net'; // Replace with a valid recipient JID
 
